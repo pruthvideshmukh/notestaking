6bf871d7b0aa7befd8438096445ad3fc7a68153d
Pruthviraj Sanjay Deshmukh <90892399+pruthvideshmukh@users.noreply.github.com>
2021-10-18 15:44:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ addBtn.addEventListener("click", function (e) {
 
     if (addTitle.value === "" || addDesc.value === "") {
         msgBox.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                <strong>Error !!</strong> All fields are required.
+                                All fields are required.
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
@@ -39,7 +39,7 @@ addBtn.addEventListener("click", function (e) {
         addDesc.value = "";
         addTitle.value = "";
         msgBox.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                                <strong>Success !!</strong> You have successfully added your note.
+                                You have successfully added your note.
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
